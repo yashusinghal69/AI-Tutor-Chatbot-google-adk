1a6859b9355152feaef08e0d20ece9a9a5200902
yashuSinghal <125557043+yashusinghal69@users.noreply.github.com>
2025-05-27 01:53:15 +0530

    update

--- start.py
+++ start.py
@@ -2,7 +2,14 @@ import os
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+
+    try:
+       uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    except Exception as e:
+        print(f"Error starting the server: {e}")
+        os.system("python -m uvicorn main:app --host 0.0.0.0 --port 8000 --reload")
+
+
    
             
             
